8798f58fa86a9e41e2fdc2fc6df90395fab0ca54
Nikolssen <ibudovichivan3@gmail.com>
2021-05-14 00:11:54 +0300

    Add: Task 2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,14 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    for (int i = 0; i < 7; i++)
+    {
+        UInt8 bitValue = n & (0x1 << i);
+        if (bitValue) {
+            result |= 1;
+        }
+        result <<= 1;
+    }
+    return result;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,35 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    for (NSNumber *number in numbersArray) {
+        int intValue = number.intValue;
+        if (intValue > 0xFF) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+        }
+        if (intValue < 0) {
+            return @"Negative numbers are not valid for input.";
+        }
+    }
+    int fourth = 0;
+    int third = 0;
+    int second = 0;
+    int first = 0;
+    switch (numbersArray.count) {
+        case 4:
+            fourth = [(NSNumber*)[numbersArray objectAtIndex:3] intValue];
+        case 3:
+            third = [(NSNumber*)[numbersArray objectAtIndex:2] intValue];
+        case 2:
+            second = [(NSNumber*)[numbersArray objectAtIndex:1] intValue];
+        case 1:
+            first = [(NSNumber*)[numbersArray objectAtIndex:0] intValue];
+            break;
+        default:
+            return @"";;
+    }
+    NSString* string = [NSString stringWithFormat:@"%d.%d.%d.%d", first, second, third, fourth];
+    return string;
 }
 
 @end
